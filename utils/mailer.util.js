33ad145f333ce6c36505845118c37e8b0cb5efb5
Vineet Singraul <singraulvineet943@gmail.com>
2026-08-17 22:58:17 +0530

    Fix Otp Conflict

--- utils/mailer.util.js
+++ utils/mailer.util.js
@@ -3,6 +3,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     service:"gmail",
+    family: 4,
     auth:{
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
